73615ac34651bf0ac759911ce91454bb8ce73bae
NKNN-dev <Grachev0613@gmail.com>
2021-03-26 21:19:48 +0300

    second commit

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'NTIxNzI3MzA1NjUxOTc4Mjgw.XA6Wag.xANOQPlTZ0JcSk3zp6J6qczJ6ns',
+        prefix: '!',
+        activity: 'egor pidor',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
